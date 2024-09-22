4b7c77b4f364d1a73311afa2aa95d43858e3ef66
James-wasonga <jameswasonga22@gmail.com>
2024-09-22 17:59:24 +0300

    arrays in python

---
+++ arrays.py
@@ -0,0 +1,23 @@
+#arrays in python are referred to lists
+
+cars = ["BMW","Volvo","Benz"]
+
+
+x = cars[0]
+print(x) #print the first car element in the array
+
+ #adding element into the array
+cars.append("Tx")
+print(cars) #printing the array
+
+z = len(cars)
+print(z)
+
+#for in loop in array
+
+#poping an element from an array
+cars.pop(1)
+
+for i in cars:
+    print(i)
+
