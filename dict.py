5c3a0c3e398ef2ba3c2b74e7563d5741327f29ea
James-wasonga <jameswasonga22@gmail.com>
2024-09-24 01:14:28 +0300

    dictionaries

--- dict.py
+++ dict.py
@@ -1,5 +1,17 @@
+#dict stores data
+
 thisdict = {
     "brand" : "BMW",
     "model" : "Mustang",
-    "year" : 1964
+    "year" : 1964,
+    "year" : 2000
 }
+
+print(thisdict["year"])
+print(len(thisdict))
+print(type(thisdict))
+
+#creating dictionary based on a constructor 
+
+thisdict = dict(name = "James",age = 20, country = "Norway")
+print(thisdict)
