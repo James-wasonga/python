75137fa30796580f24bd1a442b7c0d97afc3b4be
James-wasonga <jameswasonga22@gmail.com>
2024-09-24 18:19:37 +0300

    create a nested dictionary

---
+++ nested_dict.py
@@ -0,0 +1,42 @@
+myfriend = {
+    "child1" : {
+        "name": "James Wasonga",
+        "Year": 2004  
+    },
+    
+    "child2" : {
+        "name": "Erick Ondiwa",
+        "Year": 2005
+    },
+    
+    "child3": {
+        "name" : "Felix Awere",
+        "Year" : 2006
+    },
+    
+}
+
+mybest = myfriend["child1"]["name"]
+print(mybest)
+
+person1 = {
+    "name": "John",
+    "Year": 2004  
+    },
+    
+person2 = {
+    "name": "Jack",
+    "Year": 2005
+    },
+    
+person3 = {
+    "name" : "Linet",
+    "Year" : 2006
+    },
+
+myfamily = {
+    "person1" : person1,
+    "person2" : person2,
+    "person3" : person3
+}
+    
