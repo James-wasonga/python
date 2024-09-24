3f7d9a8b06ce8b2d9b8e72ad852cee021ca9cfe4
James-wasonga <jameswasonga22@gmail.com>
2024-09-24 18:14:05 +0300

    dictionary

--- dict.py
+++ dict.py
@@ -15,3 +15,26 @@ print(type(thisdict))
 
 thisdict = dict(name = "James",age = 20, country = "Norway")
 print(thisdict)
+
+
+# creating a dict using a costructor
+# dict()
+
+myfruit = dict(name = "mango", place = "Kisumu", number = 30 )
+print(myfruit["name"])
+
+myfruit["color"] = "pink"
+myfruit["taste"] = "perfect" 
+print(myfruit)
+
+myfruit.pop("taste")
+del myfruit["color"]
+print(myfruit)
+
+
+# a copy can't be made by "dict2 = dict1 " since the second dict will be a referrenced to the first
+# so incase of change of dict1 the second one also changes so there is no copy but a reference to the other 
+
+myfruit2 = myfruit.copy()
+print(myfruit2)
+
