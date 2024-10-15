9db9b85f78122a90022f2891049deb9e5d2f4266
James-wasonga <jameswasonga22@gmail.com>
2024-10-16 00:02:23 +0300

    redeclare the variables

--- concate.py
+++ concate.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 arr1 =  np.array([1,2,3])
-arr2 = np.array([4,5,6])
+arr3 = np.array([4,5,6])
 
 
-arr = np.concatenate((arr1, arr2))
+arr = np.concatenate((arr1, arr3))
 print(arr)
